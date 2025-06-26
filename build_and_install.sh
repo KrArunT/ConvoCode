fbff4dc8a08f8158521e02245eadb330ea0fb1e2
KrArunT <145754819+KrArunT@users.noreply.github.com>
2025-06-26 15:43:06 +0530

    Update build_and_install.sh

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,3 +1,3 @@
 npm install -g vsce
-vsce package              # produces ollama-chat-0.0.1.vsix
-code --install-extension ollama-chat-0.0.1.vsix
+vsce package  # produces convocode-0.0.1.vsix
+code --install-extension convocode-0.0.1.vsix
